3de78d74e4b55e88c98f82740cef7c938e966e8e
Debraj Kundu <kundudebraj4272@gmail.com>
2024-04-13 20:21:25 +0530

    NetworkManager-dispatcher Corrected

--- ddns-service/corpdk-update-ddns.dispatcher.sh
+++ ddns-service/corpdk-update-ddns.dispatcher.sh
@@ -2,8 +2,12 @@
 
 # Get the interface name
 INTERFACE=$1
-
 # Get the reason for the script being called (up, down, etc.)
 ACTION=$2
 
 echo "CorpDK update DDNS Check: ${INTERFACE} -> ${ACTION}"
+
+# dhcp6-change
+if [[ "${ACTION}" == "dhcp6-change" ]]; then
+    systemctl start corpdk-update-ddns.service
+fi
